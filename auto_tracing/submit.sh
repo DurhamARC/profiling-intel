a096deed8af9398993a5b954f3658156ceba5e6a
Mark Turner <mark.turner@durham.ac.uk>
2022-07-07 14:46:10 +0100

    Simplify SLURM scripts

--- auto_tracing/submit.sh
+++ auto_tracing/submit.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name itac_example
 #SBATCH --time=0:02:0
 #SBATCH --ntasks=2
-#SBATCH --cpus-per-task=2
-#SBATCH -o itac_example.out
-#SBATCH -e itac_example.err
-#SBATCH -p test
+#SBATCH --partition test
 
 module load oneapi intelmpi itac
 
 mpiicpc itac_test.cpp -o itac_test
 
-mpirun -trace -np 2 ./itac_test
+mpirun -trace ./itac_test
 
 # To view profile run:
 #     traceanalyzer itac_test.stf
